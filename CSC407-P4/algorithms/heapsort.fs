08cd84cbfb813da66161dd2d950b01c2ca920e1a
Conner Martin <55250241+Martinc11@users.noreply.github.com>
2021-04-18 14:04:09 -0400

    Garbage heap

--- CSC407-P4/algorithms/heapsort.fs
+++ CSC407-P4/algorithms/heapsort.fs
@@ -1,4 +1,38 @@
 ﻿module CSC407_P4.algorithms.heapsort
 
-let heapsort (arr : int[]) =
+open CSC407_P4.algorithms
+open bubblesort
+
+(*let swap (arr: int[]) (index1: int) (index2: int) =
+    let temp = arr.[index1]
+    arr.[index1] <- arr.[index2]
+    arr.[index2] <- temp
+    0*)
+
+let rec heapify (arr: int[]) (n: int) (i: int) : int[]=
+    let mutable largest: int = i
+    let left = 2 * i + 1
+    let right = 2 * i + 2
+    if (left < n && arr.[left] > arr.[largest]) then
+        largest <- left
+    elif (right < n && arr.[right] > arr.[largest]) then
+        largest <- right
+    if (largest <> i) then
+        swap (arr, i, largest) |> ignore
+        heapify arr n largest
+        else arr
     arr
+    
+    
+    
+    
+    
+let heapsort (arr: int[]) =
+    let n:int = arr.Length
+    for i = n/2 - 1 to 0 do
+        heapify arr n i |> ignore
+    for i = n to 0 do
+        swap (arr, i, 0) |> ignore
+        heapify arr i  0 |> ignore
+    arr
+    
